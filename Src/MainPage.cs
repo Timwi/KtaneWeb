ad143fbe406ab11f1c690b5619daba794eb70fcf
Timwi <github@timwi.de>
2017-03-03 06:47:42 +0100

    Some extra links and directory paths at the bottom of the main page.

--- Src/MainPage.cs
+++ Src/MainPage.cs
@@ -129,17 +129,25 @@ namespace KtaneWeb
                         new DIV { class_ = "links" }._(new A { href = "/json", accesskey = "j" }._("See JSON".Accel('J'))),
                         new DIV { class_ = "credits" }._("Icons by lumbud84 and samfun123."),
                         new DIV { class_ = "extra-links" }._(
-                            new H3("Controls to highlight elements in HTML manuals:"),
+                            new H3("Controls to highlight elements in HTML manuals"),
                             new TABLE { class_ = "highlighting-controls" }._(
                                 new TR(new TH("Control (Windows)"), new TH("Control (Mac)"), new TH("Function")),
                                 new TR(new TD("Ctrl+Click"), new TD("Command+Click"), new TD("Highlight a table column")),
                                 new TR(new TD("Shift+Click"), new TD("Shift+Click"), new TD("Highlight a table row")),
                                 new TR(new TD("Alt+Click or Ctrl+Shift+Click"), new TD("Command+Shift+Click"), new TD("Highlight a table cell or highlight an item in a list"))),
-                            new H3("Additional resources:"),
+                            new H3("Additional resources"),
                             new UL(
                                 new LI(new A { href = "https://www.dropbox.com/s/paluom4wlogjdl0/ModsOnlyManual_Sorted_A-Z.pdf?dl=0" }._("Rexkix’s Sorted A–Z manual (mods only)")),
                                 new LI(new A { href = "https://www.dropbox.com/s/4bkfwoa4d7p0a7z/ModsOnlyManual_Sorted_A-Z_with_Cheat_Sheets.pdf?dl=0" }._("Rexkix’s Sorted A–Z manual with cheat sheets (mods only)")),
-                                new LI(new A { href = "More/Output%20Log%20Reader.html" }._("samfun123’s output log analyzer"))))))));
+                                new LI(new A { href = "More/Output%20Log%20Reader.html" }._("samfun123’s output log analyzer")),
+                                new LI(new A { href = "https://docs.google.com/document/d/1zObWfLI8RMiNL1b6AXfiy4cwjGD9H3oStPiZaEOS5Lc" }._("On the Subject of Entering the World of Mods (by Rexkix)"))),
+                            new H3("Default file locations"),
+                            new DL(
+                                new DT("Output log (Windows, Steam):"), new DD(new CODE(@"C:\Program Files (x86)\Steam\steamapps\common\Keep Talking and Nobody Explodes\ktane_Data\output_log.txt")),
+                                new DT("Output log (Windows, Oculus):"), new DD(new CODE(@"C:\Program Files (x86)\Oculus\Software\steel-crate-games-keep-talking-and-nobody-explodes\Keep Talking and Nobody Explodes\ktane_Data\output_log.txt")),
+                                new DT("Output log (Mac):"), new DD(new CODE(@"~/Library/Logs/Unity/Player.log")),
+                                new DT("Screenshots (Windows, Steam):"), new DD(new CODE(@"C:\Program Files (x86)\Steam\userdata\<some number>\760\remote\341800\screenshots")),
+                                new DT("Screenshots (Mac, Steam):"), new DD(new CODE(@"~/Library/Application Support/Steam/userdata/<some number>/760/remote/341800/screenshots"))))))));
         }
     }
 }
