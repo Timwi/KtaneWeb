a76b10a8cc1a516007ec89813f8193836e80780a
Timwi <github@timwi.de>
2017-05-18 21:25:48 +0200

    Ability to invoke a `git pull` on the content directory.

---
+++ Src/Pull.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using RT.Servers;
+using RT.Util;
+
+namespace KtaneWeb
+{
+    public sealed partial class KtanePropellerModule
+    {
+        private HttpResponse pull(KtaneWebConfigEntry config)
+        {
+            var cmd = new CommandRunner();
+            cmd.Command = "git pull";
+            cmd.WorkingDirectory = config.BaseDir;
+            var output = new StringBuilder();
+            cmd.StdoutText += str => output.Append(str);
+            cmd.StderrText += str => output.Append(str);
+            cmd.StartAndWait();
+            return HttpResponse.PlainText(output.ToString());
+        }
+    }
+}
