987a534df822c07acad5a12b6a35b12b59457304
Timwi <github@timwi.de>
2017-06-25 04:45:09 +0200

    Allow hastebin in proxy (some Twitch Plays hosters use it to provide the logfiles)

--- Src/Proxy.cs
+++ Src/Proxy.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using RT.Servers;
 using RT.Util;
 
@@ -6,10 +7,12 @@ namespace KtaneWeb
 {
     public sealed partial class KtanePropellerModule
     {
+        private static string[] _proxyAllowedUrlPrefixes = new[] { "https://cdn.discordapp.com/", "https://hastebin.com/raw/" };
+
         private HttpResponse proxy(HttpRequest req)
         {
             var url = req.Url.Path.Substring(1);
-            if (!url.StartsWith("https://cdn.discordapp.com/"))
+            if (!_proxyAllowedUrlPrefixes.Any(url.StartsWith))
                 return HttpResponse.PlainText("", HttpStatusCode._403_Forbidden);
             try { return HttpResponse.PlainText(new HClient().Get(url).DataString); }
             catch (Exception e) { return HttpResponse.PlainText($"{e.Message} ({e.GetType().FullName})", HttpStatusCode._500_InternalServerError); }
