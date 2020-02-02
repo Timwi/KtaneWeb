7b5308df13ad8eae2e3005f1313a346841fa7322
Timwi <github@timwi.de>
2020-02-02 18:02:58 +0100

    Minor code reformat; add try/catch around the HClient.Get call

--- Src/ModuleInfoCache.cs
+++ Src/ModuleInfoCache.cs
@@ -47,90 +47,95 @@ namespace KtaneWeb
                         var rows = (iconFiles.Length + cols - 1) / cols;
                         var coords = new Dictionary<string, (int x, int y)>();
 
-                        using (var bmp = new Bitmap(w * cols, h * rows))
+                        using var bmp = new Bitmap(w * cols, h * rows);
+                        using (var g = Graphics.FromImage(bmp))
                         {
-                            using (var g = Graphics.FromImage(bmp))
+                            for (int i = 0; i < iconFiles.Length; i++)
                             {
-                                for (int i = 0; i < iconFiles.Length; i++)
-                                {
-                                    using (var icon = new Bitmap(iconFiles[i].FullName))
-                                        g.DrawImage(icon, w * (i % cols), h * (i / cols));
-                                    coords.Add(Path.GetFileNameWithoutExtension(iconFiles[i].Name), (i % cols, i / cols));
-                                }
-                            }
-                            using (var mem = new MemoryStream())
-                            {
-                                bmp.Save(mem, ImageFormat.Png);
-                                _moduleInfoCache = new ModuleInfoCache { IconSpritePng = mem.ToArray() };
-                                _moduleInfoCache.IconSpriteMd5 = MD5.Create().ComputeHash(_moduleInfoCache.IconSpritePng).ToHex();
-
-                                // Load TP data from the spreadsheet
-                                var entries = new HClient().Get("https://spreadsheets.google.com/feeds/list/1WEzVOKxOO5CDGoqAHjJKrC-c-ZGgsTPRLXBCs8RrAwU/1/public/values?alt=json").DataJson["feed"]["entry"].GetList();
-
-                                var modules = new DirectoryInfo(_config.ModJsonDir)
-                                    .EnumerateFiles("*.json", SearchOption.TopDirectoryOnly)
-                                    .ParallelSelect(4, file =>
-                                    {
-                                        try
-                                        {
-                                            var origFile = File.ReadAllText(file.FullName);
-                                            var modJson = JsonDict.Parse(origFile);
-                                            var mod = ClassifyJson.Deserialize<KtaneModuleInfo>(modJson);
-
-                                            // Merge in TP data
-                                            bool equalNames(string nameA, string nameB) => nameA.Replace('’', '\'') == nameB.Replace('’', '\'');
-                                            var entry = entries.FirstOrDefault(entry => equalNames(entry["gsx$modulename"]["$t"].GetString(), mod.Name));
-                                            if (entry != null)
-                                            {
-                                                mergeTPData(mod, entry);
-                                                modJson = (JsonDict) ClassifyJson.Serialize(mod);
-                                            }
-#if DEBUG
-                                            var newJson = (JsonDict) ClassifyJson.Serialize(mod);
-                                            var newJsonStr = newJson.ToStringIndented();
-                                            if (newJsonStr != origFile)
-                                                File.WriteAllText(file.FullName, newJsonStr);
-                                            modJson = newJson;
-#endif
-
-                                            return (modJson, mod, file.LastWriteTimeUtc).Nullable();
-                                        }
-                                        catch (Exception e)
-                                        {
-#if DEBUG
-                                            Console.WriteLine(file);
-                                            Console.WriteLine(e.Message);
-                                            Console.WriteLine(e.GetType().FullName);
-                                            Console.WriteLine(e.StackTrace);
-#endif
-                                            _logger.Exception(e);
-                                            return null;
-                                        }
-                                    })
-                                    .WhereNotNull()
-                                    .ToArray();
-                                _moduleInfoCache.Modules = modules.Select(m => m.mod).ToArray();
-                                _moduleInfoCache.ModulesJson = new JsonDict { { "KtaneModules", modules.Select(m => m.modJson).ToJsonList() } };
-                                _moduleInfoCache.LastModifiedUtc = modules.Max(m => m.LastWriteTimeUtc);
-
-                                var modJsons = modules.Where(tup => tup.mod.TranslationOf == null).Select(tup =>
-                                {
-                                    var (modJson, mod, _) = tup;
-                                    modJson["Sheets"] = _config.EnumerateSheetUrls(mod.Name, modules.Select(m => m.mod.Name).Where(m => m.Length > mod.Name.Length && m.StartsWith(mod.Name)).ToArray());
-                                    var (x, y) = coords.Get(mod.Name, (x: 0, y: 0));
-                                    modJson["X"] = x;   // note how this gets set to 0,0 for icons that don’t exist, which are the coords for the blank icon
-                                    modJson["Y"] = y;
-                                    return modJson;
-                                }).ToJsonList();
-
-                                var iconDirs = Enumerable.Range(0, _config.DocumentDirs.Length).SelectMany(ix => new[] { _config.OriginalDocumentIcons[ix], _config.ExtraDocumentIcons[ix] }).ToJsonList();
-                                var disps = _displays.Select(d => d.id).ToJsonList();
-                                var filters = _filters.Select(f => f.ToJson()).ToJsonList();
-                                var selectables = _selectables.Select(sel => sel.ToJson()).ToJsonList();
-                                var souvenir = EnumStrong.GetValues<KtaneModuleSouvenir>().ToJsonDict(val => val.ToString(), val => val.GetCustomAttribute<KtaneSouvenirInfoAttribute>().Apply(attr => new JsonDict { { "Tooltip", attr.Tooltip }, { "Char", attr.Char.ToString() } }));
-                                _moduleInfoCache.ModuleInfoJs = $@"initializePage({modJsons},{iconDirs},{_config.DocumentDirs.ToJsonList()},{disps},{filters},{selectables},{souvenir},'{_moduleInfoCache.IconSpriteMd5}');";
+                                using (var icon = new Bitmap(iconFiles[i].FullName))
+                                    g.DrawImage(icon, w * (i % cols), h * (i / cols));
+                                coords.Add(Path.GetFileNameWithoutExtension(iconFiles[i].Name), (i % cols, i / cols));
                             }
                         }
+                        using var mem = new MemoryStream();
+                        bmp.Save(mem, ImageFormat.Png);
+                        _moduleInfoCache = new ModuleInfoCache { IconSpritePng = mem.ToArray() };
+                        _moduleInfoCache.IconSpriteMd5 = MD5.Create().ComputeHash(_moduleInfoCache.IconSpritePng).ToHex();
+
+                        // Load TP data from the spreadsheet
+                        JsonList entries;
+                        try
+                        {
+                            entries = new HClient().Get("https://spreadsheets.google.com/feeds/list/1WEzVOKxOO5CDGoqAHjJKrC-c-ZGgsTPRLXBCs8RrAwU/1/public/values?alt=json").DataJson["feed"]["entry"].GetList();
+                        }
+                        catch (Exception e)
+                        {
+                            _logger.Exception(e);
+                            entries = new JsonList();
+                        }
+
+                        var modules = new DirectoryInfo(_config.ModJsonDir)
+                            .EnumerateFiles("*.json", SearchOption.TopDirectoryOnly)
+                            .ParallelSelect(4, file =>
+                            {
+                                try
+                                {
+                                    var origFile = File.ReadAllText(file.FullName);
+                                    var modJson = JsonDict.Parse(origFile);
+                                    var mod = ClassifyJson.Deserialize<KtaneModuleInfo>(modJson);
+
+                                    // Merge in TP data
+                                    static bool equalNames(string nameA, string nameB) => nameA.Replace('’', '\'') == nameB.Replace('’', '\'');
+                                    var entry = entries.FirstOrDefault(entry => equalNames(entry["gsx$modulename"]["$t"].GetString(), mod.Name));
+                                    if (entry != null)
+                                    {
+                                        mergeTPData(mod, entry);
+                                        modJson = (JsonDict) ClassifyJson.Serialize(mod);
+                                    }
+#if DEBUG
+                                    var newJson = (JsonDict) ClassifyJson.Serialize(mod);
+                                    var newJsonStr = newJson.ToStringIndented();
+                                    if (newJsonStr != origFile)
+                                        File.WriteAllText(file.FullName, newJsonStr);
+                                    modJson = newJson;
+#endif
+
+                                    return (modJson, mod, file.LastWriteTimeUtc).Nullable();
+                                }
+                                catch (Exception e)
+                                {
+#if DEBUG
+                                    Console.WriteLine(file);
+                                    Console.WriteLine(e.Message);
+                                    Console.WriteLine(e.GetType().FullName);
+                                    Console.WriteLine(e.StackTrace);
+#endif
+                                    _logger.Exception(e);
+                                    return null;
+                                }
+                            })
+                            .WhereNotNull()
+                            .ToArray();
+                        _moduleInfoCache.Modules = modules.Select(m => m.mod).ToArray();
+                        _moduleInfoCache.ModulesJson = new JsonDict { { "KtaneModules", modules.Select(m => m.modJson).ToJsonList() } };
+                        _moduleInfoCache.LastModifiedUtc = modules.Max(m => m.LastWriteTimeUtc);
+
+                        var modJsons = modules.Where(tup => tup.mod.TranslationOf == null).Select(tup =>
+                        {
+                            var (modJson, mod, _) = tup;
+                            modJson["Sheets"] = _config.EnumerateSheetUrls(mod.Name, modules.Select(m => m.mod.Name).Where(m => m.Length > mod.Name.Length && m.StartsWith(mod.Name)).ToArray());
+                            var (x, y) = coords.Get(mod.Name, (x: 0, y: 0));
+                            modJson["X"] = x;   // note how this gets set to 0,0 for icons that don’t exist, which are the coords for the blank icon
+                            modJson["Y"] = y;
+                            return modJson;
+                        }).ToJsonList();
+
+                        var iconDirs = Enumerable.Range(0, _config.DocumentDirs.Length).SelectMany(ix => new[] { _config.OriginalDocumentIcons[ix], _config.ExtraDocumentIcons[ix] }).ToJsonList();
+                        var disps = _displays.Select(d => d.id).ToJsonList();
+                        var filters = _filters.Select(f => f.ToJson()).ToJsonList();
+                        var selectables = _selectables.Select(sel => sel.ToJson()).ToJsonList();
+                        var souvenir = EnumStrong.GetValues<KtaneModuleSouvenir>().ToJsonDict(val => val.ToString(), val => val.GetCustomAttribute<KtaneSouvenirInfoAttribute>().Apply(attr => new JsonDict { { "Tooltip", attr.Tooltip }, { "Char", attr.Char.ToString() } }));
+                        _moduleInfoCache.ModuleInfoJs = $@"initializePage({modJsons},{iconDirs},{_config.DocumentDirs.ToJsonList()},{disps},{filters},{selectables},{souvenir},'{_moduleInfoCache.IconSpriteMd5}');";
                     }
         }
 
@@ -175,9 +180,7 @@ namespace KtaneWeb
                 return;
 
             if (decimal.TryParse(scoreString, out decimal score))
-            {
                 tp.Score ??= score;
-            }
         }
     }
 }
