c98cc869d4665e08e44ae9fc25679ce55314bbdf
Timwi <github@timwi.de>
2020-12-13 16:53:25 +0100

    Ignore table is now an HTML file in KtaneContent

--- Src/KtanePropellerModule.cs
+++ Src/KtanePropellerModule.cs
@@ -50,7 +50,6 @@ namespace KtaneWeb
                     new UrlMapping(path: "/find-log", handler: findLogfile),
                     new UrlMapping(path: "/generate-json", handler: generateJson),
                     new UrlMapping(path: "/iconsprite", handler: req => HttpResponse.Create(getModuleInfoCache().IconSpritePng, "image/png")),
-                    new UrlMapping(path: "/ignore-table", handler: ignoreTable),
                     new UrlMapping(path: "/sitemap", specificPath: true, handler: sitemapXml),
 
                     new UrlMapping(path: "/puzzles", handler: req => puzzles(req, _config.Puzzles, session)),
