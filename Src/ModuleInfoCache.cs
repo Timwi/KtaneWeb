759c9a06c7927d5e14c912e813afe53acd585887
Timwi <github@timwi.de>
2021-07-06 06:16:39 +0200

    Show errors in ContactInfo.json as exception in console

--- Src/ModuleInfoCache.cs
+++ Src/ModuleInfoCache.cs
@@ -203,7 +203,12 @@ namespace KtaneWeb
             var filters = _filters.Select(f => f.ToJson()).ToJsonList();
             var selectables = _selectables.Select(sel => sel.ToJson()).ToJsonList();
             var souvenir = EnumStrong.GetValues<KtaneModuleSouvenir>().ToJsonDict(val => val.ToString(), val => val.GetCustomAttribute<KtaneSouvenirInfoAttribute>().Apply(attr => new JsonDict { { "Tooltip", attr.Tooltip }, { "Char", attr.Char.ToString() } }));
-            moduleInfoCache.ModuleInfoJs = $@"initializePage({modJsons},{iconDirs},{_config.DocumentDirs.ToJsonList()},{disps},{filters},{selectables},{souvenir},{moduleLoadExceptions},{File.ReadAllText(Path.Combine(_config.BaseDir, "ContactInfo.json"))});";
+
+            JsonValue contactInfoJson = new JsonDict();
+            try { contactInfoJson = JsonValue.Parse(File.ReadAllText(Path.Combine(_config.BaseDir, "ContactInfo.json"))); }
+            catch (Exception e) { moduleLoadExceptions.Add("ContactInfo.json error: " + e.Message); }
+
+            moduleInfoCache.ModuleInfoJs = $@"initializePage({modJsons},{iconDirs},{_config.DocumentDirs.ToJsonList()},{disps},{filters},{selectables},{souvenir},{moduleLoadExceptions},{contactInfoJson});";
             _moduleInfoCache = moduleInfoCache;
         }
 
