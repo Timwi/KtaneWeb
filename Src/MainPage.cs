22596de73daecfb7d8320d9fb711a321e69fa2ec
Timwi <github@timwi.de>
2021-08-03 15:19:09 +0200

    Highlighter keyboard shortcuts clarification

--- Src/MainPage.cs
+++ Src/MainPage.cs
@@ -265,7 +265,8 @@ namespace KtaneWeb
                                     new TR(new TH("Highlight a table column"), new TD("Ctrl+Click (Windows)", new BR(), "Command+Click (Mac)")),
                                     new TR(new TH("Highlight a table row"), new TD("Shift+Click")),
                                     new TR(new TH("Highlight a table cell or an item in a list"), new TD("Alt+Click (Windows)", new BR(), "Ctrl+Shift+Click (Windows)", new BR(), "Command+Shift+Click (Mac)")),
-                                    new TR(new TH("Change highlighter color"), new TD("Alt+0 through Alt+9")))),
+                                    new TR(new TH("Change highlighter color"), new TD("Alt+0 through Alt+9 (digits)")),
+                                    new TR(new TH("Additional options"), new TD("Alt+O (letter)")))),
                             new H3("Default file locations"),
                             new H4("Windows"),
                             new TABLE { class_ = "file-locations" }._(
