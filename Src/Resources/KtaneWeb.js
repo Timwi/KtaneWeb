8a05b0f3f9e84b8fb67c65dc034cdcef86654d2e
Timwi <github@timwi.de>
2023-08-12 13:54:42 +0200

    Periodic Table visual fix

--- Src/Resources/KtaneWeb.js
+++ Src/Resources/KtaneWeb.js
@@ -794,7 +794,7 @@ function initializePage(modules, initIcons, initDocDirs, initDisplays, initFilte
                         el('div', `symbol ${mod.DefuserDifficulty}`, el('span', 'inner', mod.Symbol || '??')),
                         el("img", "mod-icon", { src: `Icons/${mod.X === 0 && mod.Y === 0 ? 'blank' : encodeURIComponent(mod.FileName ?? mod.Name)}.png` }),
                         el('div', 'name', el('div', 'inner', mod.localName)),
-                        el('div', 'tpscore', mod.TwitchPlays?.ScoreString),
+                        el('div', 'tpscore', mod.TwitchPlays?.ScoreString ?? ''),
                         el('div', 'souvenir', souvenirStatuses[(mod.Souvenir && mod.Souvenir.Status) || 'Unexamined']),
                         manualSelector);
                     setCompatibilityTooltip(a, mod);
