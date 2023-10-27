17b0f2905f88eab1019844a7ee44fe6800fc7ca9
Timwi <github@timwi.de>
2023-10-27 15:11:42 +0200

    Do "run" by default if command line is empty.

--- Src/Program.cs
+++ Src/Program.cs
@@ -6,6 +6,9 @@ namespace KtaneWeb
     {
         static int Main(string[] args)
         {
+            if (args.Length == 0)
+                args = new[] { "run" };
+
             CommandLineBase cmd;
             try
             {
