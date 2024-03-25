6d7117a3a4207f8182831dbcc692526c793e9517
Timwi <github@timwi.de>
2024-03-25 15:32:46 +0100

    Move the URL handler to a separate .cs file to make it easier to edit the list of URLs. Added bonus is that it doesn’t recreate it afresh for every request

--- Src/KtanePropellerModule.cs
+++ Src/KtanePropellerModule.cs
@@ -18,89 +18,7 @@ namespace KtaneWeb
 
         private KtaneWebConfig _config;
 
-        public override HttpResponse Handle(HttpRequest request)
-        {
-            var auth = _config.UsersFile?.Apply(file => new FileAuthenticator(file, _ => request.Url.WithPath("").ToHref(), "Repository of Manual Pages"));
-
-            return new KtaneWebSession(_config).EnableAutomatic(request, session =>
-            {
-                var mergedPdfsFs = new FileSystemHandler(_config.MergedPdfsDir);
-
-                var resolver = new UrlResolver(
-#if DEBUG
-                    new UrlMapping(path: "/js", specificPath: true, handler: req => HttpResponse.File(_config.JavaScriptFile, "text/javascript; charset=utf-8")),
-                    new UrlMapping(path: "/css", specificPath: true, handler: req => HttpResponse.File(_config.CssFile, "text/css; charset=utf-8")),
-#else
-                    new UrlMapping(path: "/js", specificPath: true, handler: req => HttpResponse.JavaScript(Resources.Js)),
-                    new UrlMapping(path: "/css", specificPath: true, handler: req => HttpResponse.Css(Resources.Css)),
-#endif
-
-                    new UrlMapping(path: "/", specificPath: true, handler: mainPage),
-                    new UrlMapping(path: "/profile", handler: generateProfileZip),
-                    new UrlMapping(path: "/json", handler: req =>
-                    {
-                        if (req.Url.Path == "/raw")
-                            return HttpResponse.Json(_moduleInfoCache.ModulesJson, HttpStatusCode._200_OK, new HttpResponseHeaders { AccessControlAllowOrigin = "*" });
-                        else if (req.Url.Path == "/flavourtext")
-                            return HttpResponse.Json(_moduleInfoCache.ModulesJsonFlavourText, HttpStatusCode._200_OK, new HttpResponseHeaders { AccessControlAllowOrigin = "*" });
-                        else if (req.Url.Path == "/startingline")
-                            return HttpResponse.Json(_moduleInfoCache.ModulesJsonStartingLine, HttpStatusCode._200_OK, new HttpResponseHeaders { AccessControlAllowOrigin = "*" });
-                        else
-                            return HttpResponse.Redirect(req.Url.WithPathParent().WithPathOnly("/JSON" + req.Url.Path));
-                    }),
-                    new UrlMapping(path: "/pull", handler: pull),
-                    new UrlMapping(path: "/ManualLastUpdated", handler: ManualLastUpdated),
-                    new UrlMapping(path: "/proxy", handler: proxy),
-                    new UrlMapping(path: "/merge-pdf", handler: mergePdfs),
-                    new UrlMapping(path: "/pdf-diag", handler: pdfDiag),
-                    new UrlMapping(path: "/upload-log", handler: uploadLogfile),
-                    new UrlMapping(path: "/find-log", handler: findLogfile),
-                    new UrlMapping(path: "/generate-json", handler: generateJson),
-                    new UrlMapping(path: "/iconsprite", handler: req => HttpResponse.Create(_moduleInfoCache.IconSpritePng, "image/png")),
-                    new UrlMapping(path: "/sitemap", specificPath: true, handler: sitemapXml),
-
-                    new UrlMapping(path: "/puzzles", handler: req => puzzles(req, _config.Puzzles, session)),
-
-                    new UrlMapping(path: "/Unfinished", handler: unfinished, skippable: true),
-                    new UrlMapping(path: "/MergedPdfs", handler: mergedPdfsFs.Handle),
-                    new UrlMapping(path: "/Logfiles", handler: logFileHandler),
-
-                    // Shortcut URLs
-                    new UrlMapping(path: "/lfa", handler: req => HttpResponse.Redirect(req.Url.WithPathParent().WithPathOnly("/More/Logfile Analyzer.html"))),
-                    new UrlMapping(path: "/faq", handler: req => HttpResponse.Redirect(req.Url.WithPathParent().WithPathOnly("/More/Glossary.html"))),
-                    new UrlMapping(path: "/pe", handler: req => HttpResponse.Redirect(req.Url.WithPathParent().WithPathOnly("/More/Profile Editor.html"))),
-                    new UrlMapping(path: "/mse", handler: req => HttpResponse.Redirect(req.Url.WithPathParent().WithPathOnly("/More/Mode Settings Editor.html"))),
-
-                    // Redirects from old file names
-                    new UrlMapping(path: "/More/FAQs.html", handler: req => HttpResponse.Redirect(req.Url.WithPathParent().WithPath("/More/Glossary.html"))),
-
-                    // Default fallback: file system handler or PDF generator
-                    new UrlMapping(handler: req => pdf(req) ?? new FileSystemHandler(_config.BaseDir, new FileSystemOptions
-                    {
-                        MimeTypeOverrides = new Dictionary<string, string>
-                        {
-                            ["wav"] = "audio/wav"
-                        }
-                    }).Handle(req))
-                );
-
-                if (auth != null)
-                    resolver.Add(new UrlMapping(path: "/auth", handler: req => auth.Handle(req, session.Username, user =>
-                    {
-                        session.Username = user;
-                        lock (_config)
-                        {
-                            if (user == null)
-                                _config.Sessions.Remove(session.SessionID);
-                            else
-                                _config.Sessions[session.SessionID] = user;
-                            saveConfig();
-                        }
-                    })));
-
-                return resolver.Handle(request);
-            });
-        }
+        public override HttpResponse Handle(HttpRequest request) => new KtaneWebSession(_config).EnableAutomatic(request, session => _urlResolver.Handle(request));
 
         public override void Init()
         {
@@ -274,6 +192,7 @@ namespace KtaneWeb
 
             generateTranslationCache();
             generateModuleInfoCache();
+            InitUrlResolver();
         }
 
         private void saveConfig()
---
+++ Src/Urls.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using RT.Servers;
+using RT.Util;
+
+namespace KtaneWeb
+{
+    partial class KtanePropellerModule
+    {
+        private UrlResolver _urlResolver;
+
+        private void InitUrlResolver()
+        {
+            var debug = false;
+#if DEBUG
+            debug = true;
+#endif
+
+            _urlResolver = new UrlResolver(
+                new UrlMapping(path: "/js", specificPath: true, handler: req => debug ? HttpResponse.File(_config.JavaScriptFile, "text/javascript; charset=utf-8") : HttpResponse.JavaScript(Resources.Js)),
+                new UrlMapping(path: "/css", specificPath: true, handler: req => debug ? HttpResponse.File(_config.CssFile, "text/css; charset=utf-8") : HttpResponse.Css(Resources.Css)),
+
+                new UrlMapping(path: "/", specificPath: true, handler: mainPage),
+                new UrlMapping(path: "/profile", handler: generateProfileZip),
+                new UrlMapping(path: "/json", handler: req =>
+                {
+                    if (req.Url.Path == "/raw")
+                        return HttpResponse.Json(_moduleInfoCache.ModulesJson, HttpStatusCode._200_OK, new HttpResponseHeaders { AccessControlAllowOrigin = "*" });
+                    else if (req.Url.Path == "/flavourtext")
+                        return HttpResponse.Json(_moduleInfoCache.ModulesJsonFlavourText, HttpStatusCode._200_OK, new HttpResponseHeaders { AccessControlAllowOrigin = "*" });
+                    else if (req.Url.Path == "/startingline")
+                        return HttpResponse.Json(_moduleInfoCache.ModulesJsonStartingLine, HttpStatusCode._200_OK, new HttpResponseHeaders { AccessControlAllowOrigin = "*" });
+                    else
+                        return HttpResponse.Redirect(req.Url.WithPathParent().WithPathOnly("/JSON" + req.Url.Path));
+                }),
+                new UrlMapping(path: "/pull", handler: pull),
+                new UrlMapping(path: "/ManualLastUpdated", handler: ManualLastUpdated),
+                new UrlMapping(path: "/proxy", handler: proxy),
+                new UrlMapping(path: "/merge-pdf", handler: mergePdfs),
+                new UrlMapping(path: "/pdf-diag", handler: pdfDiag),
+                new UrlMapping(path: "/upload-log", handler: uploadLogfile),
+                new UrlMapping(path: "/find-log", handler: findLogfile),
+                new UrlMapping(path: "/generate-json", handler: generateJson),
+                new UrlMapping(path: "/iconsprite", handler: req => HttpResponse.Create(_moduleInfoCache.IconSpritePng, "image/png")),
+                new UrlMapping(path: "/sitemap", specificPath: true, handler: sitemapXml),
+
+                new UrlMapping(path: "/puzzles", handler: req => new KtaneWebSession(_config).EnableAutomatic(req, session => puzzles(req, _config.Puzzles, session))),
+
+                new UrlMapping(path: "/Unfinished", handler: unfinished, skippable: true),
+                new UrlMapping(path: "/MergedPdfs", handler: new FileSystemHandler(_config.MergedPdfsDir).Handle),
+                new UrlMapping(path: "/Logfiles", handler: logFileHandler),
+
+                // Shortcut URLs
+                new UrlMapping(path: "/lfa", handler: req => HttpResponse.Redirect(req.Url.WithPathParent().WithPathOnly("/More/Logfile Analyzer.html"))),
+                new UrlMapping(path: "/faq", handler: req => HttpResponse.Redirect(req.Url.WithPathParent().WithPathOnly("/More/Glossary.html"))),
+                new UrlMapping(path: "/pe", handler: req => HttpResponse.Redirect(req.Url.WithPathParent().WithPathOnly("/More/Profile Editor.html"))),
+                new UrlMapping(path: "/mse", handler: req => HttpResponse.Redirect(req.Url.WithPathParent().WithPathOnly("/More/Mode Settings Editor.html"))),
+
+                // Redirects from old file names
+                new UrlMapping(path: "/More/FAQs.html", handler: req => HttpResponse.Redirect(req.Url.WithPathParent().WithPath("/More/Glossary.html"))),
+
+                // Default fallback: file system handler or PDF generator
+                new UrlMapping(handler: req => pdf(req) ?? new FileSystemHandler(_config.BaseDir, new FileSystemOptions
+                {
+                    MimeTypeOverrides = new Dictionary<string, string>
+                    {
+                        ["wav"] = "audio/wav"
+                    }
+                }).Handle(req)),
+
+                // Auth
+                new UrlMapping(path: "/auth", handler: req =>
+                {
+                    var auth = _config.UsersFile?.Apply(file => new FileAuthenticator(file, _ => req.Url.WithPath("").ToHref(), "Repository of Manual Pages"));
+                    return auth == null ? null : new KtaneWebSession(_config).EnableAutomatic(req, session => auth.Handle(req, session.Username, user =>
+                    {
+                        session.Username = user;
+                        lock (_config)
+                        {
+                            if (user == null)
+                                _config.Sessions.Remove(session.SessionID);
+                            else
+                                _config.Sessions[session.SessionID] = user;
+                            saveConfig();
+                        }
+                    }));
+                }));
+        }
+    }
+}
